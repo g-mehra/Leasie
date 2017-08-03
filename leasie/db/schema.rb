b2e1668f48ed45184d9fa5924ef7693f43d61a70
g-mehra <gmehra@alumni.uwo.ca>
2017-08-03 18:47:23 -0400

    added photo model

--- leasie/db/schema.rb
+++ leasie/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803224523) do
+ActiveRecord::Schema.define(version: 20170803224634) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
