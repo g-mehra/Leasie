1a5bd0d3999e2388b23c21928bd57c1da5d9e19d
Pawel Parafianowicz <pawelp@outlook.com>
2017-09-15 18:55:59 -0400

    Update task list with user stories

--- leasie/test/models/property_test.rb
+++ leasie/test/models/property_test.rb
@@ -4,4 +4,17 @@ class PropertyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'two properties exist' do
+    assert_equal 2, Property.all.size
+  end
+  
+  def setup
+    @property = Property.create('123 Fake St', 'description', '$1,400', 1, 2, false, true, 'included', 2)
+  end
+
+  # test 'three properties exist' do
+  #   assert_equal 3, Property.all.size
+  # end
+
 end
