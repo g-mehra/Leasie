c5c5474cf6eb5d62d3fb3a487fefd1290c5bd6c7
Pawel Parafianowicz <pawelp@outlook.com>
2017-09-21 19:07:22 -0400

    Add other fields to sign-up on device, create device registrations controller

--- leasie/app/models/user.rb
+++ leasie/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
-  has_many :properties
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
+  has_many :owned_properties, class_name: "Property"
   has_many :appointments, through: :properties
 end
