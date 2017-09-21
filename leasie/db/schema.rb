0b8cda60517f752a1052b58ab05d67fdcf0a602f
frankburke333 <frank.burke@gmail.com>
2017-09-21 11:16:05 -0400

    added in owner distinction for properties and added user_id to properties db

--- leasie/db/schema.rb
+++ leasie/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901174248) do
+ActiveRecord::Schema.define(version: 20170921133709) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "datetime"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170901174248) do
     t.string   "type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
