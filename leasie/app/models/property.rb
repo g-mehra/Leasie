91a284bf576adc64386bc3f073d5d167db46b861
frankburke333 <frank.burke@gmail.com>
2017-10-24 10:01:53 -0400

    added carrierwave with local upload ability. Multiple files coming next

--- leasie/app/models/property.rb
+++ leasie/app/models/property.rb
@@ -2,7 +2,9 @@ class Property < ApplicationRecord
   belongs_to :user
   has_many :appointments
   has_many :photos
-  
+  mount_uploader :photos, AvatarUploader
+
+
 
 
 end
--- leasie/db/schema.rb
+++ leasie/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803231106) do
+ActiveRecord::Schema.define(version: 20171023203949) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "datetime"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170803231106) do
     t.string   "type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "photos"
   end
 
   create_table "users", force: :cascade do |t|
